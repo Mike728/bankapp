e5d23f8ad2232da01a50b34d5691bcd3baf26826
Michał <michal.prokop9@yahoo.pl>
2019-02-23 19:46:44 +0100

    Initial commit

---
+++ src/main/java/info/mike/bankapp/BankappApplication.java
@@ -0,0 +1,13 @@
+package info.mike.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankappApplication.class, args);
+	}
+
+}
